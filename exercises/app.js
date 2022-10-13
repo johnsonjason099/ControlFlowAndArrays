48368f038c85158f4ae6cd7173ae5b4a5310c581
johnsonjason099 <johnsonjason099@gmail.com>
2022-10-13 16:30:01 -0600

    second commit

--- exercises/app.js
+++
@@ -1,9 +0,0 @@
-
-let num = ();
-if (num >= 100); 
-console.log ('greater');
-else if (num <= 100);
-console.log ("you've got less than 100");
-else if (num );
-}
-let letterGrade = 90
---
+++ exercises/controlFlow/app.js
@@ -0,0 +1,15 @@
+console.log ("Hello World");
+let num = (55);
+if (num > 100){
+console.log ('greater');    
+} 
+
+else if (num < 100){
+console.log ("you've got less than 100");
+}
+
+else {
+console.log ("you have a negative number");    
+}
+
+
