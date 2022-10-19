8e5ee567893ee4ddbec3d3798e29c6a314a36631
johnsonjason099 <johnsonjason099@gmail.com>
2022-10-19 10:50:23 -0600

    ninjas

---
+++ OOJSNinja/OOJSNinja.js
@@ -0,0 +1,47 @@
+console.log(Ninja);
+//video1
+// var names = ['ryu', 'pink', 'glibber']
+// 0: 
+// names.lengthnames.sort()
+// window.innerWidth
+// name.length
+// var name2 = new String('ryu')
+// name2.typeof
+
+// var userOne = {
+//     email: 'ryu@ninjas.com',
+//     name: 'Ryu'
+//     login(){
+//         console.log(this.email, 'has logged in');
+//     },
+//     logout();
+//         console.log(this.email, 'has logged out');}
+// ;
+// //these should be on console on live server
+// // console.log(userOne.name)
+// // userOne.login();
+// userOne.name = "YOSHI"
+// // userOne['email']
+// // userOne['name'] = "mario"
+// // var prop = "name"
+// // userOne[prop]
+// // prop = 'email'
+// // userOne.prop
+// // userOne.name
+// // userOne.age = 25
+// // userOne
+// // userOne.age
+// // userOne.loginfo = function(){}; //is possible
+// //creating multiple objects we will make classes
+// var userFour = new User();
+
+class User{
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
+}
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariocart.com', 'Yoshi');
+    console.log(userOne);
+    console.log(userTwo)
--- exercises/Functions/functions.js
+++ exercises/Functions/functions.js
@@ -6,3 +6,16 @@ favShows.forEach(show=>console.log(show + "is a cool movie"));
 let firstShow = favShows[0]
 
 console.log(firstShow);
+
+function areBothEven(n1,n2);{
+    console.log(1,2)
+    return !(ne %2) && !(n2 %2); {
+        }
+}
+console.log(areBothEven(3,7));
+
+let maria = getDevObject("Maria", "HTML", "CSS")
+return {
+    devName: name,
+    jobSkills: skills
+}
