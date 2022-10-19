1259b10f6dbfca7b31e2c776ccd683434b3fef3c
johnsonjason099 <johnsonjason099@gmail.com>
2022-10-18 20:43:04 -0600

    week2 day2

--- bjectsAndOrcs/objectsAndOrcs.js
+++ bjectsAndOrcs/objectsAndOrcs.js
@@ -1,22 +1,22 @@
-//const adventurer = {
+const adventurer = {
     name: "timothy",
-    hitpoints: 10,
-    belongings: ["sword", "axe", "tums"]
+   hitpoints: 10,
+    belongings: ["sword", "axe", "tums"],
     companion: {
         name: "peanuts",
-        type: "chichi"
+        type: "chichi",
         companion: {
-            name: "tapeworm"
+            name: "tapeworm",
             type: "parasite"
     }
 }
 }
     
-//console.log(adventurer.companion);
-//console.log(adventurer.companion.type);
-//console.log(adventurer.companion.name);
+console.log(adventurer.companion);
+console.log(adventurer.companion.type);
+console.log(adventurer.companion.name);
 
-//const movies = [
+const movies = [
     { title: "Toy Story"},
     { title: "Mall Cop"},
     { title: "harry meets Sallo"},
@@ -30,69 +30,69 @@
  //   console.log(movies[i].title);
  // }
 
-//  const foo = {
-//     someArray: [1, 2, 3],
-//     someObject: {
-//         someProp:'oh, Hi, Billy'
-//     },
-//     someMethod: () => {
-//         console.log("inside of method");
-//     }
-//  }
-//  console.log(foo.someObject.someProp[0]);
-//  console.log(foo.someArray[0]); // 1
-//  console.log(foo.someMethod());
+ //const foo: = {
+ //   someArray: [1, 2, 3],
+ //   someObject: {
+ //       someProp:'oh, Hi, Billy'
+    
+    someMethod = () => {
+        console.log("inside of method");
+    }
 
-//  const foo = [
-//     {prop: 'weeeee!'},
-//     2,
-//  ]
+ //console.log(foo.someObject.someProp[0]);
+ //console.log(foo.someArray[0]); // 1
+ //console.log(foo.someMethod());
+
+ //const foo = [
+ //   {prop: 'weeeee!'},
+ //   2,
+ //  ]
 
 //     //other
 
-//     const foo = [
-//         [1,2,3,4],
-//         [5,6,7,8],
-//         [9,10,11,12]
-//     ]
-// console.log(foo[1][2]);//7
+    const foo = [
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12]
+    ]
+console.log(foo[1][2]);//7
 
-// const foo = [
-//     1,
-//     "Hi",
-//     ()=> {
-//         console.log("fun")
-//     };   
+const foo = [
+    1,
+    "Hi",
+    ()=> {
+        console.log("fun")
+    };   
 
-// ]
+]
 
-// const player = {
-//     name: "Josh the Great",
-//     health: 1000,
-//     power: 1000,
-//     stamina: 1000
-// }
-// const player = {
-//     name: "Josh the Great",
-//     health: 1000,
-//     power: 1000,
-//     stamina: 1000
-// }
-// const createEnemy = (nameIs, HealthIs, staminaIs) => {
-//     const createEmeny = {
-//         name: "Groot",
-//         health: 1000,
-//         power: 1000,
-//         stamina: 1000
-//     }   
-// }
-// const createPlayer = (nameIs, HealthIs, staminaIs) => {
-//     const createPlayer = {
-//         name: "Groot",
-//         health: 1000,
-//         power: 1000,
-//         stamina: 1000
-//     }   
+const player = {
+    name: "Josh the Great",
+    health: 1000,
+    power: 1000,
+    stamina: 1000
+}
+const player = {
+    name: "Josh the Great",
+    health: 1000,
+    power: 1000,
+    stamina: 1000
+}
+const createEnemy = (nameIs, HealthIs, staminaIs) => {
+    const createEmeny = {
+        name: "Groot",
+        health: 1000,
+        power: 1000,
+        stamina: 1000
+    }   
+}
+const createPlayer = (nameIs, HealthIs, staminaIs) => {
+    const createPlayer = {
+        name: "Groot",
+        health: 1000,
+        power: 1000,
+        stamina: 1000
+    }   
 
 class Character {
     //constructor
@@ -105,7 +105,14 @@ class Character {
         this.hair = 'gray'
         this.lovesCats = lovesCats;
         this.lovesDogs = lovesDogs;
+    }Greetings " )
+    //set method
+    setHair(hairColor) {
+        this.hair = hairColor
     }
+classyGreeting(otherClassyCharacter) {
+    console.log("Greetings + otherClassyCharacter.name + "!"")
+}
  greet(otherCharcter){
     console.log("Hi");
  }
@@ -115,9 +122,71 @@ class Character {
 }
 
 const player1 = new Character("Ariel", 97, "green",  );
-const enemy = new Character();
+const enemy = new Character("Sauron", 1000, "green");
 
 console.log(player1.greet("Billy"));
 console.log(player1.greet("Black Adam"));
-
+player1.hair = 'supernova red'
 console.log(player1);
+player1.setHair('pearl');
+console.log(enemy.classyGreeting(player1));
+
+parent classes
+pasted inform
+class Hobbit extends Character {
+    steal () {
+        console.log("let's get away!")
+    }
+    greet (otherCharacter) {
+        console.log("Hobbit says hello to ${otherCharacter")
+    }
+    smite() {
+        super.smite();
+            this.steal();
+            console.log("another one");
+    }
+}
+const frodo = new Hobbit("Mr Baggins", 33, "blue", "brown");
+console.log(frodo);
+console.log(frodo.smite());
+console.log(frodo.steal());
+console.log(frodo.greet("Sam"));
+
+//creating a factory
+
+class Car {
+    constructor (make, serialNumber){
+        this.serialNumber = serialNumber;
+        this.make = make;
+
+    }
+    drive() {
+        console.log('vroom vroom');
+
+    }
+}
+class Factory {
+    construtor(company){
+        this.company = company;
+        this.cars = {}
+            }
+}
+gerericCar() {
+    const newCar = new Car(this.company, this.AbortSignal.length);
+    this.cars.push(newCar);
+    findCar (index){
+        return this.cars[index]
+    }
+}
+const newCar = new Car()
+const tesla = new Factory("Tesla");
+tesla.generateCar();
+tesla.generateCar();
+console.log(tesla);
+console.log(tesla.findCar(0))
+
+const porche = new Factory('Porche');
+porche.generateCar();
+porche.generateCar();
+console.log(porche);
+console.log(porche.findCar(1));
